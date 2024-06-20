dca37d66229144a739c9de4285596de35cc7b9bc
itc-s23009 <s23009@std.it-college.ac.jp>
2024-06-20 14:30:08 +0900

    不足分を記入

--- src/main/kotlin/jp/ac/it_college/std/s23009/messageboard/domain/repository/UsersRepository.kt
+++ src/main/kotlin/jp/ac/it_college/std/s23009/messageboard/domain/repository/UsersRepository.kt
@@ -3,7 +3,9 @@ package jp.ac.it_college.std.s23009.messageboard.domain.repository
 import jp.ac.it_college.std.s23009.messageboard.domain.model.Users
 
 interface UsersRepository {
-    fun findByEmail(email: String): Users?
-
     fun findById(id: Long): Users?
+    fun findByEmail(email: String): Users?
+    fun createUser(user: Users): Users
+    fun updateUser(user: Users): Users
+    fun deleteUser(id: Long)
 }
--- src/main/kotlin/jp/ac/it_college/std/s23009/messageboard/infrastructurs/database/repository/UsersRepositoryImpl.kt
+++ src/main/kotlin/jp/ac/it_college/std/s23009/messageboard/infrastructurs/database/repository/UsersRepositoryImpl.kt
@@ -9,17 +9,49 @@ import org.springframework.stereotype.Repository
 
 @Repository
 class UsersRepositoryImpl : UsersRepository {
-    override fun findByEmail(email: String): Users? {
-        return transaction {
-            UsersEntity.find {
-                UsersTable.email eq email
-            }.singleOrNull()?.toUser()
-        }
-    }
 
     override fun findById(id: Long): Users? {
         return transaction {
-            UsersEntity.findById(id)?.toUser()
+            val userEntity = UsersEntity.findById(id)
+            userEntity?.toUser()
+        }
+    }
+
+    override fun findByEmail(email: String): Users? {
+        return transaction {
+            val userEntity = UsersEntity.find { UsersTable.email eq email }.firstOrNull()
+            userEntity?.toUser()
+        }
+    }
+
+    override fun createUser(user: Users): Users {
+        return transaction {
+            val newUserEntity = UsersEntity.new {
+                this.viewName = user.viewName
+                this.email = user.email
+                this.password = user.password // 通常はパスワードのハッシュ化が必要
+            }
+            newUserEntity.toUser()
+        }
+    }
+
+    override fun updateUser(user: Users): Users {
+        return transaction {
+            val userEntity = UsersEntity.findById(user.id)
+                ?: throw IllegalArgumentException("User not found with id: ${user.id}")
+
+            userEntity.apply {
+                viewName = user.viewName
+                email = user.email
+                // パスワードの更新などがあれば適宜追加する
+            }
+            userEntity.toUser()
+        }
+    }
+
+    override fun deleteUser(id: Long) {
+        transaction {
+            UsersEntity.findById(id)?.delete()
         }
     }
 }
